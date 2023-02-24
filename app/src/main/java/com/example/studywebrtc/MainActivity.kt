76623598b3adec91deaff571212f2c731143091d
Parkduksung <duksung1234@naver.com>
2023-02-24 15:31:58 +0900

    Compose Camera 키기

--- app/src/main/java/com/example/studywebrtc/MainActivity.kt
+++ app/src/main/java/com/example/studywebrtc/MainActivity.kt
@@ -1,19 +1,44 @@
 package com.example.studywebrtc
 
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import android.view.ViewGroup
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
 import com.example.studywebrtc.ui.theme.StudyWebRTCTheme
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.PermissionRequired
+import com.google.accompanist.permissions.rememberPermissionState
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import java.io.File
+import java.util.concurrent.Executor
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 class MainActivity : ComponentActivity() {
+
+
+    @OptIn(ExperimentalPermissionsApi::class, ExperimentalCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
 
 
         setContent {
@@ -23,9 +48,179 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    Permission(
+                        permissionNotAvailableContent = {
+                            //권한 미획득
+                        },
+                        content = {
+                            //권한 획득
+                            CameraCapture(
+                                lifecycleOwner = LocalLifecycleOwner.current,
+                                onImageFile = {
 
+                                })
+                        }
+                    )
                 }
             }
         }
+    }
+}
+
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun Permission(
+    permission: String = android.Manifest.permission.CAMERA,
+    rationale: String = "This permission is important for this app. Please grant the permission.",
+    permissionNotAvailableContent: @Composable () -> Unit = { },
+    content: @Composable () -> Unit = { }
+) {
+    val permissionState = rememberPermissionState(permission)
+    PermissionRequired(
+        permissionState = permissionState,
+        permissionNotGrantedContent = {
+            Rationale(
+                text = rationale,
+                onRequestPermission = { permissionState.launchPermissionRequest() }
+            )
+        },
+        permissionNotAvailableContent = permissionNotAvailableContent,
+        content = content
+    )
+}
+
+@Composable
+private fun Rationale(
+    text: String,
+    onRequestPermission: () -> Unit
+) {
+    AlertDialog(
+        onDismissRequest = { /* Don't */ },
+        title = {
+            Text(text = "Permission request")
+        },
+        text = {
+            Text(text)
+        },
+        confirmButton = {
+            Button(onClick = onRequestPermission) {
+                Text("Ok")
+            }
+        }
+    )
+}
+
+@ExperimentalPermissionsApi
+@ExperimentalCoroutinesApi
+@Composable
+fun CameraCapture(
+    lifecycleOwner: LifecycleOwner,
+    modifier: Modifier = Modifier,
+    cameraSelector: CameraSelector = CameraSelector.DEFAULT_BACK_CAMERA,
+    onImageFile: (File) -> Unit = { }
+) {
+    val context = LocalContext.current
+    Permission(
+        permission = Manifest.permission.CAMERA,
+        rationale = "You said you wanted a picture, so I'm going to have to ask for permission.",
+        permissionNotAvailableContent = {
+            Column(modifier) {
+                Text("O noes! No Camera!")
+                Spacer(modifier = Modifier.height(8.dp))
+                Button(
+                    onClick = {
+                        context.startActivity(
+                            Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                                data = Uri.fromParts("package", context.packageName, null)
+                            }
+                        )
+                    }
+                ) {
+                    Text("Open Settings")
+                }
+            }
+        }
+    ) {
+        Box(modifier = modifier) {
+            val coroutineScope = rememberCoroutineScope()
+            var previewUseCase by remember { mutableStateOf<UseCase>(Preview.Builder().build()) }
+            val imageCaptureUseCase by remember {
+                mutableStateOf(
+                    ImageCapture.Builder()
+                        .setCaptureMode(ImageCapture.CAPTURE_MODE_MAXIMIZE_QUALITY)
+                        .build()
+                )
+            }
+
+
+
+            Box(modifier) {
+                CameraPreview(
+                    modifier = modifier,
+                    onUseCase = {
+                        previewUseCase = it
+                    }
+                )
+            }
+            LaunchedEffect(previewUseCase) {
+                val cameraProvider = context.getCameraProvider()
+                try {
+                    cameraProvider.bindToLifecycle(
+                        lifecycleOwner,
+                        cameraSelector,
+                        previewUseCase,
+                        imageCaptureUseCase
+                    )
+                } catch (ex: Exception) {
+                    Log.e("CameraCapture", "Failed to bind camera use cases", ex)
+                }
+            }
+
+        }
+    }
+}
+
+
+@Composable
+fun CameraPreview(
+    modifier: Modifier = Modifier,
+    scaleType: PreviewView.ScaleType = PreviewView.ScaleType.FILL_CENTER,
+    onUseCase: (UseCase) -> Unit = { }
+) {
+    AndroidView(
+        modifier = modifier,
+        factory = { context ->
+            val previewView = PreviewView(context).apply {
+                this.scaleType = scaleType
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+            }
+            onUseCase(
+                Preview.Builder()
+                    .build()
+                    .also {
+                        it.setSurfaceProvider(previewView.surfaceProvider)
+                    }
+            )
+            previewView
+        }
+    )
+}
+
+
+val Context.executor: Executor
+    get() = ContextCompat.getMainExecutor(this)
+
+suspend fun Context.getCameraProvider(): ProcessCameraProvider = suspendCoroutine { continuation ->
+    ProcessCameraProvider.getInstance(this).also { future ->
+        future.addListener(
+            {
+                continuation.resume(future.get())
+            },
+            executor
+        )
     }
 }
